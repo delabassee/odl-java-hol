0a76d426dc1df39451296f59b036d9732efd39c1
David Delabassée <david.delabassee@gmail.com>
2021-02-06 10:20:58 +0100

    UI nows uses summary

--- src/main/resources/public/SpeakerController.js
+++ src/main/resources/public/SpeakerController.js
@@ -57,7 +57,7 @@ function showResults(data){
     $("#notFound").hide();
     data.forEach(function(speaker) {
         var item = $(renderSpeakers(speaker));
-        item.on("click", function() {
+/*        item.on("click", function() {
             var detailItem = $(renderSpeakerDetails(speaker));
             $("#home").hide();
             $("#detail").empty();                                   
@@ -69,7 +69,7 @@ function showResults(data){
                 function() {
                     $("#detail").show()
                 });
-        });
+        });*/
         $("#people").append(item);
     });
 }
@@ -119,12 +119,8 @@ function renderSpeakerDetails(speaker){
     var template = $('#detail_template').html();
     Mustache.parse(template);
     var rendered = Mustache.render(template,{
-        "id" : speaker.id,
-        "firstName" : speaker.firstName,
-        "lastName" : speaker.lastName,
-        "title" : speaker.title,
+		"speaker" : speaker.speaker,
         "company" : speaker.company,
-        "track" : speaker.track
     });
     return rendered;
 }
